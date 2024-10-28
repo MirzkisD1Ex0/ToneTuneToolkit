9843aa51fd01fffd9cea138db317b8b62edf93f9
MirzkisD1Ex0 <lovezcz13685521730@live.com>
2024-10-28 10:28:58 +0800

    1

--- Materials/WebGL/SkipUnityLogo/WebGLSkipUnityLogo.cs
+++ Materials/WebGL/SkipUnityLogo/WebGLSkipUnityLogo.cs
@@ -2,7 +2,7 @@
 using UnityEngine;
 using UnityEngine.Rendering;
 
-public class SkipUnityLogo
+public class WebGLSkipUnityLogo
 {
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
     private static void BeforeSplashScreen()
@@ -28,7 +28,3 @@ public class SkipUnityLogo
 #endif
 }
 #endif
-
-// 百度和谷歌了一下解决方法，最终是通过用国外ip来重新激活license（仍然选择个人免费版），就解决问题了。
-// 就是说只要不是国内ip去激活，就不会显示 trial version 字样了。
-// 删除它的project settings文件夹。删除这个文件夹会导致一些配置的丢失，所以如果这样做的话需要考虑把配置迁移过来。
